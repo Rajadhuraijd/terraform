71c2bbfdf2afd1d0a6c91219d0753b50ab66cef9
rajadhuraijd <rajadurai78@gmail.com>
2024-05-16 23:27:43 +0530

    2nd code

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "dev-server1" {
+    count = 2
+    instance_type = "t2.micro"
+    ami = "ami-0bb84b8ffd87024d8"
+    key_name = "privkey"
+    subnet_id = "subnet-0a611ced4615fd6e8"
+    /*security_groups = "sg-0ef5a3bf94b3e492e"*/
+
+    tags = {
+      Name = "devserver${count.index+1}"
+    }
+  
+}
